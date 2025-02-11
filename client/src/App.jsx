15a2b0f11991f93185bedf1f6b9f90ed1bdb386b
Shubham Kumar <saw.shubham@proton.me>
2025-02-11 19:47:00 +0000

    feat: toggle theme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { ToggleTheme } from './components/toggle-theme'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -24,6 +25,7 @@ function App() {
         </p>
       </div>
       <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+      <ToggleTheme />
     </>
   )
 }
